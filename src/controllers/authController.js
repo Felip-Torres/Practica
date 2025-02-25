0d17aa452b3a22e4157da28518ed0b22fd831778
Felip <156847988+Felip-Torres@users.noreply.github.com>
2025-02-25 13:17:34 +0100

    final

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -36,7 +36,6 @@ router.post('/login', async (req, res) => {
     try {
         const user = await Usuario.findOne({ where: { Email: req.query.Email, Password: req.query.Password } });
         const token = user.generateAuthToken();
-        console.log(token);
         res.status(200).json({ user, token });
     } catch (error) {
         res.status(400).json({ error: 'Unable to login: ' + error });
